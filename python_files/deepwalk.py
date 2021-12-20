d457a712ef39f5ca1a17e5ab3d06c645dbb255bb
unnadig <tjeriph@gmail.com>
2021-12-20 12:46:46 +0100

    two random walkers

--- python_files/deepwalk.py
+++ python_files/deepwalk.py
@@ -1,9 +1,12 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+import os
 
 # read data from file
-filename = 'data/ucidata-zachary/out.ucidata-zachary'
+dirname = os.path.realpath('..')
+filename = os.path.join(dirname, 'data/ucidata-zachary/out.ucidata-zachary')
 rows = []
 with open(filename) as file:
     for line in file:
@@ -18,4 +21,42 @@ for i in range(len(rows)):
 G = nx.Graph()
 G.add_edges_from(rows)
 
-# deepwalk algorithm
+def randomWalk(G, v, t):
+    visited = []
+    node = v
+    for k in range(t):
+        node = random.choice(list(G.neighbors(node)))
+        visited.append(node)
+    
+    return visited
+
+def randomWalk_adj(G, v, t):
+    A = nx.adjacency_matrix(G)
+    A = A.todense()
+    A = np.array(A, dtype=np.float64)
+    T = A/A.sum(axis=1, keepdims=True)
+
+    p = np.zeros(A.shape[0]).reshape(-1,1)
+    p[v] = 1
+    
+    visited = []
+    for k in range(t):
+        p = np.dot(T,p)
+        visited.append(np.argmax(p))
+
+    return visited
+
+def skipGram(phi, W, w):
+
+    return None
+
+def deepWalk(G, w, d, gamma, t):
+    # initialization
+    V = 0
+    # build binary tree
+    for i in range(gamma):
+        V_shuffle = 0
+        for v in v_shuffle:
+            W = randomWalk(G, v, t)
+            skipGram(phi, W, w)
+    return 0

830884bb912b2aaadc22ada7536597f945b5e44a
unnadig <tjeriph@gmail.com>
2022-01-09 17:39:50 +0100

    preprocessing done, text data includes non-words

--- new_project/main.py
+++ new_project/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
+import gensim.parsing.preprocessing as gs
 # image data
 w = 50
 W_array = np.zeros((1300,w**2))
@@ -10,7 +11,6 @@ j = 0
 for file in os.listdir():
     if file.endswith(".tiff"):
         X = plt.imread(file)
-        
         # 100 windows from each image
         for i in range(100):
             W = X[i:i+w,i:i+w].flatten()
@@ -19,10 +19,25 @@ for file in os.listdir():
         j += 1
 
 W_array = np.transpose(W_array)
-print(W_array)
 # text data
-#vectorizer = TfidfVectorizer()
-#response = vectorizer.fit_transform()
+corpus = []
+dirnames = ['sci.crypt/', 'sci.med/', 'sci.space/', 'soc.religion.christian/']
+for dirname in dirnames:
+    for file in os.listdir('20_newsgroups/'+dirname):
+        with open('20_newsgroups/'+dirname+file, encoding="utf8", errors='ignore') as f:
+            l = 0
+            for line in f:
+                # remove opening lines of document
+                if l > 15:
+                    contents = f.read()
+                    # preprocessing for iftdf
+                    filtered_text = gs.remove_stopwords(contents)
+                    #filtered_text = gs.strip_non_alphanum(filtered_text)
+                    corpus.append(filtered_text)
+                l += 1
+
+vectorizer = TfidfVectorizer()
+response = vectorizer.fit_transform(corpus)
 
 def RP(X,k):
     d,N = X.shape
@@ -30,7 +45,7 @@ def RP(X,k):
     Y = np.dot(R,X)
     return Y
 
-Y = RP(W_array, 500)
+#Y = RP(W_array, 500)
 
 def SRP(X):
     Y = 0
